8bc5df4b81afe045f96e1a5f693c4d2042404c3f
grupomvp8 <grupomvpmvp@gmail.com>
2025-06-08 18:33:06 +0000

    primeiro

---
+++ home/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const subscribeButton = document.querySelector(".subscribe-button");
+  const ctaButton = document.querySelector(".cta-button");
+
+  if (subscribeButton) {
+    subscribeButton.addEventListener("click", () => {
+      window.location.href = "assine.html";
+    });
+  }
+
+  if (ctaButton) {
+    ctaButton.addEventListener("click", () => {
+      window.location.href = "teste.html";
+    });
+  }
+});
