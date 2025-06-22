12ea207336e96d0bbf7d8be263747c1d5f1486a5
grupomvp8 <grupomvpmvp@gmail.com>
2025-06-22 09:51:26 -0300

    paginas adicionadas

--- home/assinatura/script.js
+++ home/assinatura/script.js
@@ -11,3 +11,18 @@ document.getElementById('form-assinatura').addEventListener('submit', function (
     e.preventDefault();
   }
 });
+
+document.getElementById('form-assinatura').addEventListener('submit', function (e) {
+  e.preventDefault(); // Impede envio automático
+
+  const nome = document.getElementById('nome').value.trim();
+
+  if (nome.split(' ').length < 2) {
+    alert('Por favor, insira seu nome completo (nome e sobrenome).');
+    return;
+  }
+
+  // Simula envio do formulário, salvar dados, etc.
+
+});
+
---
+++ home/home/carrimg/js.js
@@ -0,0 +1,8 @@
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  e.target.matches('.next') && slider.append(items[0]);
+}
+
+document.addEventListener('click',activate,false);
